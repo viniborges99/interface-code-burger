2f9c1247a8e3262f4a4ee37470196977e436055d
viniborges99 <borgesvinicius27@gmail.com>
2024-01-04 22:46:07 -0300

    primeiro commit, erro no prettier

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import Login from './containers/Login'
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<Login />)
