3de3e048cc36d7c3b42327127c5e60fb2133ba96
viniborges99 <borgesvinicius27@gmail.com>
2024-01-09 21:14:07 -0300

    corrigindo o erro do prettier renomeando ele

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 function Login() {
-    return (
-        <div>
-            <h1>Olá</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Olá</h1>
+    </div>
+  )
 }
+
 export default Login
